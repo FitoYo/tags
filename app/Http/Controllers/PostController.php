011091f228c51832ff6edb45055cc90679afeab0
FiTo <claudiopero@estudiopero.com>
2022-06-30 23:25:25 +0200

    backup

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        $posts = Post::orderBy('id', 'DESC')->get();
+
+        return view('index', compact('posts'));
+    }
+
+    public function store()
+    {
+        
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Conner\Tagging\Taggable;
 
 class Post extends Model
 {
     use HasFactory;
+    use Taggable;
+
+    protected $fillable = [
+        'title', 'body'
+    ];
+
 }
